316b662c7df145151f0452298ae57a063ec27f96
i bersenyov <iivanovich@axilon.ru>
2026-05-09 17:00:43 +0500

    deploy troubles

--- code/sshs1.sh
+++ code/sshs1.sh
@@ -1,2 +1,2 @@
 set -a ; source ./.env ; set +a 
-echo "$SERV1_LOGIN"@"$SERV1_IP" && sshpass -p "$SERV1_PASS" ssh "$SERV1_LOGIN"@"$SERV1_IP" 
+echo "$SERV1_LOGIN"@"$SERV1_IP" && sshpass -p "$SERV1_PASS" ssh -X "$SERV1_LOGIN"@"$SERV1_IP" 
--- code/sshs2.sh
+++ code/sshs2.sh
@@ -1,2 +1,2 @@
 set -a ; source ./.env ; set +a 
-echo "$SERV2_LOGIN"@"$SERV2_IP" && sshpass -p "$SERV2_PASS" ssh "$SERV2_LOGIN"@"$SERV2_IP" 
+echo "$SERV2_LOGIN"@"$SERV2_IP" && sshpass -p "$SERV2_PASS" ssh -X "$SERV2_LOGIN"@"$SERV2_IP" 
