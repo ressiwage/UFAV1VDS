af75d406dc2230c422b6247fe15259f4075b0793
i bersenyov <iivanovich@axilon.ru>
2026-05-10 19:18:04 +0500

    new ip resolv meth

--- code/frontend/react/docker-entrypoint.sh
+++ code/frontend/react/docker-entrypoint.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 set -e
 
+cat > /app/dist/env-config.js << EOF
+window.__ENV__ = {
+  GATEWAY_URL: "${GATEWAY_URL}",
+  GATEWAY_WS_URL: "${GATEWAY_WS_URL}",
+  HOSTNAME: ${HOSTNAME}
+};
+EOF
+
 cat > /app/public/env-config.js << EOF
 window.__ENV__ = {
   GATEWAY_URL: "${GATEWAY_URL}",
-  GATEWAY_WS_URL: "${GATEWAY_WS_URL}"
+  GATEWAY_WS_URL: "${GATEWAY_WS_URL}",
+  HOSTNAME: ${HOSTNAME}
 };
 EOF
 
 echo "Runtime config:"
+cat /app/public/env-config.js
 cat /app/dist/env-config.js
 
 exec npm run preview -- --host 0.0.0.0 --port 4173
