a86f8af01a6effa9e52c58c731c8aaeda81be6f9
i bersenyov <iivanovich@axilon.ru>
2026-05-16 22:55:41 +0500

    репорты в параллели: 402, 335. также удалил матюки

--- code/FUCKING-KILL-EVERYBODY.sh
+++ code/FUCKING-KILL-EVERYBODY.sh
@@ -1,4 +1,3 @@
-# 1. Остановить всё нахуй
 docker compose down -v
 
 # 2. Удалить все контейнеры (все, не только из этого проекта)
@@ -10,7 +9,6 @@ docker rmi -f $(docker images -q) 2>/dev/null
 # 4. Удалить все тома (volumes) с кешами
 docker volume rm $(docker volume ls -q) 2>/dev/null
 
-# 5. Очистить билд-кеш (самое важное блядь)
 docker builder prune -af
 
 # 6. Системная чистка всего подряд
